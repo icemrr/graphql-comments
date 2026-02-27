2da92f12658cffd3b94d70056a78b03362fd420d
icemrr <vasko9723@mail.ru>
2026-02-27 13:16:29 +0300

    Update handler.go

--- internal/gql/handler.go
+++ internal/gql/handler.go
@@ -7,7 +7,7 @@ import (
 	"github.com/graphql-go/handler"
 )
 
-// NewHandler создает HTTP handler для GraphQL с GraphiQL
+
 func NewHandler(schema *graphql.Schema) http.Handler {
 	return handler.New(&handler.Config{
 		Schema:   schema,
