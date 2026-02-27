23c896055f0fd6fa7b70d113fe6e3b2329265520
icemrr <vasko9723@mail.ru>
2026-02-27 13:14:44 +0300

    Update models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,31 +1,31 @@
 package models
 
-// Post представляет собой пост/статью, к которой пишутся комментарии
+
 type Post struct {
-	ID       string     `json:"id"`       // Уникальный идентификатор поста
-	Title    string     `json:"title"`    // Заголовок поста
-	Content  string     `json:"content"`  // Содержимое поста
-	Comments []*Comment `json:"comments"` // Список комментариев первого уровня
+	ID       string     `json:"id"`       
+	Title    string     `json:"title"`    
+	Content  string     `json:"content"`  
+	Comments []*Comment `json:"comments"` 
 }
 
-// Comment представляет собой комментарий, который может быть ответом на пост или другой комментарий
+
 type Comment struct {
-	ID       string     `json:"id"`       // Уникальный идентификатор комментария
-	PostID   string     `json:"postId"`   // ID поста, к которому относится комментарий
-	ParentID *string    `json:"parentId"` // ID родительского комментария (nil для комментариев первого уровня)
-	Content  string     `json:"content"`  // Текст комментария
-	Replies  []*Comment `json:"replies"`  // Дочерние комментарии (ответы на этот комментарий)
+	ID       string     `json:"id"`       
+	PostID   string     `json:"postId"`  
+	ParentID *string    `json:"parentId"` 
+	Content  string     `json:"content"` 
+	Replies  []*Comment `json:"replies"`  
 }
 
-// CreatePostInput - входные данные для создания поста
+
 type CreatePostInput struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
-// CreateCommentInput - входные данные для создания комментария
+
 type CreateCommentInput struct {
-	PostID   string  `json:"postId"`   // Обязательно
-	ParentID *string `json:"parentId"` // Необязательно, может быть nil
-	Content  string  `json:"content"`  // Обязательно
+	PostID   string  `json:"postId"`   
+	ParentID *string `json:"parentId"` 
+	Content  string  `json:"content"`  
 }
